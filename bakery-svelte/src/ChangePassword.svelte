4cd541508a9ed48337641fd610aee8f16b7c07cf
AgathaB <agathabb07@gmail.com>
2020-11-18 13:37:09 +0100

    my profile component

--- bakery-svelte/src/ChangePassword.svelte
+++ bakery-svelte/src/ChangePassword.svelte
@@ -1,15 +1,27 @@
 <script>
-    import {user} from './store.js'
+    //import {user} from './store.js';
+    import {componente} from '.store.js';
 
-var newPassword = getASecureRandomPassword();
+//var newPassword = getASecureRandomPassword();
 
-$user.updatePassword(newPassword).then(function() {
+/*$user.updatePassword(newPassword).then(function() {
   // Update successful.
 }).catch(function(error) {
   // An error happened.
-});
+});*/
+
+
+function goProfile() {
+  $componente = "MyProfile";
+}
 
 </script>
+
+
+<div class="nav-bar">
+  <span class="back-button" on:click={goProfile}><i class="fas fa-arrow-left"></i></span>
+  <h1 class="profile">Cambiar contraseña</h1>
+</div>
 
 <p>Contraseña Actual</p>
 <input type="text" placeholder="Contraseña actual">
@@ -22,5 +34,10 @@ $user.updatePassword(newPassword).then(function() {
 
 
 <style>
-
+.nav-bar {
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 3px 7px -3px gray;
+}
 </style>
--- bakery-svelte/src/MyProfile.svelte
+++ bakery-svelte/src/MyProfile.svelte
@@ -10,6 +10,10 @@ function goBack() {
 function goChangePass() {
   $componente = "ChangePassword";
 }
+
+function goAddNumber() {
+  $componente = "PhoneNumber";
+}
 </script>
 
 
@@ -17,17 +21,22 @@ function goChangePass() {
       <span class="back-button" on:click={goBack}><i class="fas fa-arrow-left"></i></span>
       <h1 class="profile">Mi Perfil</h1>
 </div>
+  <div class="user-container">
       {#if ($user && $user.displayName)}
+          <span><i class="fas fa-user"></i></span>
           <p class="user-name">{$user.displayName}</p>
-          <p class="user-email">{$user.email}</p>
-          <p>Cambiar mi contraseña <span on:click={goChangePass}><i class="fas fa-chevron-right"></i></span></p>
+          <span><i class="fas fa-envelope"></i></span><p class="user-email">{$user.email}</p>
+          <span><i class="fas fa-lock"></i></span><p>Cambiar mi contraseña <span on:click={goChangePass}><i class="fas fa-chevron-right"></i></span></p>
+          <span><i class="fas fa-mobile-alt"></i></span><p>Añadir número de teléfono<span on:click={goAddNumber}><i class="fas fa-chevron-right"></i></span></p>
       {:else if ($user && $user.email)}
-          <input type="text" placeholder="Nombre">
-          <p class="user-email">{$user.email}</p>
-          <p>Cambiar mi contraseña <span on:click={goChangePass}><i class="fas fa-chevron-right"></i></span></p>
+          <span><i class="fas fa-user"></i></span><input type="text" placeholder="Nombre">
+          <span><i class="fas fa-envelope"></i></span><p class="user-email">{$user.email}</p>
+          <span><i class="fas fa-lock"></i></span><p>Cambiar mi contraseña <span on:click={goChangePass}><i class="fas fa-chevron-right"></i></span></p>
+          <span><i class="fas fa-mobile-alt"></i></span><p>Añadir número de teléfono<span on:click={goAddNumber}><i class="fas fa-chevron-right"></i></span></p>
       {:else}
         <p>Inicia sesión</p>
       {/if}
+    </div>
 
 
 <style>
@@ -40,6 +49,11 @@ function goChangePass() {
   padding: 15px;
   display: flex;
   align-items: center;
+  box-shadow: 0 3px 7px -3px gray;
+}
+
+.user-container {
+  padding-top: 30px;
 }
 
 .profile {
